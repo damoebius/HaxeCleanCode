05d0158e1d52c38b71f99174b2eb527399cb0e56
damoebius <tintin33$>
2017-03-05 16:16:51 +0100

    Parse file

--- src/CleanAction.java
+++ src/CleanAction.java
@@ -1,16 +1,66 @@
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
+import com.intellij.psi.PsiDocumentManager;
+import com.intellij.psi.PsiFile;
+import org.apache.log4j.Logger;
+import org.tamina.intellij.codecleaner.Parser;
+import org.tamina.intellij.codecleaner.entry.ClassContentsEntry;
+import org.tamina.intellij.codecleaner.settings.RearrangerSettings;
+import org.tamina.intellij.codecleaner.util.CommentUtil;
+
+import java.util.List;
 
 /**
  * Created by damo on 03/03/17.
  */
 public class CleanAction extends AnAction {
+
+    private static final Logger logger = Logger.getLogger("CleanAction");
+
     public void actionPerformed(AnActionEvent event) {
         Project project = event.getData(PlatformDataKeys.PROJECT);
-        String txt= Messages.showInputDialog(project, "What is your name?", "Input your name", Messages.getQuestionIcon());
-        Messages.showMessageDialog(project, "Hello, " + txt + "!\n I am glad to see you.", "Information", Messages.getInformationIcon());
+        Editor editor = event.getData(PlatformDataKeys.EDITOR);
+        final Document    document    = editor.getDocument();
+        final PsiFile     psiFile     = event.getData(PlatformDataKeys.PSI_FILE);
+        rearrangeDocument(project,psiFile,document);
     }
+
+    public final void rearrangeDocument(final Project            project ,
+                                        final PsiFile psiFile ,
+                                        final Document document )
+    {
+        logger.debug("enter rearrangeDocument");
+        final RearrangerSettings settings = RearrangerSettings.getDefaultSettings();
+        CommentUtil u = new CommentUtil(settings);
+        final Parser p = new Parser(project, settings, psiFile);
+        final List<ClassContentsEntry> outerClasses           = p.parseOuterLevel();
+        /*if (outerClasses.size() > 0)
+        {
+            final Mover     m                      = new Mover(outerClasses, settings);
+            final List<IRuleInstance> resultRuleInstances = m.rearrangeOuterClasses();
+            boolean rearrange = true;
+            if (settings.isAskBeforeRearranging()) {
+                FileStructurePopup fsp = new FileStructurePopup(settings, resultRuleInstances, psiFile);
+                rearrange = fsp.displayRearrangement();
+            }
+            if (rearrange) {
+                final Emitter   e                      = new Emitter(psiFile, resultRuleInstances, document);
+                e.emitRearrangedDocument();
+            }
+        }
+        logger.debug("respacing document");
+        PsiDocumentManager.getInstance(project).commitDocument(document);
+        Spacer spacer = new Spacer(project, psiFile, document, settings);
+        if (spacer.respace()) {
+            PsiDocumentManager.getInstance(project).commitDocument(document);
+        }  */
+        logger.debug("exit rearrangeDocument");
+    }
+
 }
